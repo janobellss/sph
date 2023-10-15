e3db33b6f6777820e59f59990b43ea521f33a2b3
Jano Quiambao <jquiambao@swanleap.com>
2023-10-15 17:52:19 +0800

    Create footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="absolute bottom-2 right-2">
+      <p className="text-xs">
+        Created for demo purposes only. No Copyright Infringement Intended.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
